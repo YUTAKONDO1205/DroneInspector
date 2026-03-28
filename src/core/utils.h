4dd2237815c635140b8105dc5485ceb0e7e542e6
Yuta Kondo <2210930014u@hiro.kindai.ac.jp>
2026-03-28 20:57:26 +0900

    photo

--- src/core/utils.h
+++ src/core/utils.h
@@ -6,4 +6,3 @@
 void makeImageName(char* out, size_t out_size, uint32_t frame_id);
 void safeCopyRunId(char* dst, size_t dst_size, const char* src);
 float calcNorm3(float x, float y, float z);
-float calcNorm3(float x, float y, float z);
